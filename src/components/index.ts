6d564f4b2f4860870368504c2e2eef5656026144
hyankelwin <hyankelwin@hotmail.com>
2025-06-17 15:25:17 -0300

    feat: index export all components

---
+++ src/components/index.ts
@@ -0,0 +1,6 @@
+export { default as Avatar } from './Avatar';
+export { default as Breadcrumbs } from './Breadcrumbs';
+export { default as Button } from './Button';
+export { default as Icon } from './Icon';
+export { default as Checkbox } from './Inputs/Checkbox';
+export { default as Select } from './Inputs/Select';
